ada41f09a7bd801832ff67598be460aa3570a66a
Annie <overheads165@gmail.com>
2022-12-08 23:41:34 +0100

    done

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,14 @@
 const cells = document.querySelectorAll(".cell");
 let score = 0;
 let goblinPosition = Math.floor(Math.random() * cells.length);
+let missed = 0;
 
 
 cells[goblinPosition].classList.add("goblin");
 
 setInterval(() => {
   const goblinCell = document.querySelector(".goblin");
-  
+
   if (goblinCell) {
     goblinPosition = Array.from(cells).indexOf(goblinCell);
     goblinCell.classList.remove("goblin");
@@ -21,6 +22,13 @@ setInterval(() => {
   }
 
   cells[randomCell].classList.add("goblin");
+  missed++;
+  console.log(missed + "missed");
+  if(missed === 5){
+    alert("Game over: Your score is " + score);
+    missed = 0;
+    score = 0;
+  }
 }, 1000);
 
 cells.forEach((cell) => {
@@ -28,6 +36,7 @@ cells.forEach((cell) => {
     console.log(score);
     if (cell.classList.contains("goblin")) {
       score++;
+      missed--;
       cell.classList.remove("goblin");
     }
   });

99af716f5978496be0feefc23adec11682a658ed
Annie <overheads165@gmail.com>
2022-12-08 23:10:07 +0100

    hammer hit

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,34 @@
-// гоблин рандомно появляется в рандомной клетке
+//создаем переменную для хранения количества очков
 const cells = document.querySelectorAll(".cell");
+let score = 0;
+let goblinPosition = Math.floor(Math.random() * cells.length);
 
-const randomCell = Math.floor(Math.random() * cells.length);
-cells[randomCell].classList.add("goblin");
+
+cells[goblinPosition].classList.add("goblin");
 
 setInterval(() => {
   const goblinCell = document.querySelector(".goblin");
+  
+  if (goblinCell) {
+    goblinPosition = Array.from(cells).indexOf(goblinCell);
+    goblinCell.classList.remove("goblin");
+  }
 
-  // найти индекс клетки гоблина в массиве cells
-  const goblinCellIndex = Array.from(cells).indexOf(goblinCell);
-  
-  goblinCell.classList.remove("goblin");
-  
   // рандомно выбрать индекс клетки куда переместить гоблина чтобы он не оказался в той же клетке
   let randomCell = Math.floor(Math.random() * cells.length);
-  while (randomCell === goblinCellIndex) {
+  while (randomCell === goblinPosition) {
     randomCell = Math.floor(Math.random() * cells.length);
   }
 
   cells[randomCell].classList.add("goblin");
-}, 100000);
+}, 1000);
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    console.log(score);
+    if (cell.classList.contains("goblin")) {
+      score++;
+      cell.classList.remove("goblin");
+    }
+  });
+});
